85fb6fde27cb3f99e5127f751e8232a9035cff97
Aliaksandr Kudrautsau <sany199574@gmail.com>
2022-08-03 15:22:15 +0300

    Добавлена бизнес логика. Entities

---
+++ src/PowerfulSpace.PollingSystem/PowerfulSpace.PollingSystem.Entities/Poll.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerfulSpace.PollingSystem.Entities
+{
+    public class Poll
+    {
+        public string QuestionText { get; set; } = null!;
+    }
+
+}
---
+++ src/PowerfulSpace.PollingSystem/PowerfulSpace.PollingSystem.Entities/PollAnswer.cs
@@ -0,0 +1,12 @@
+﻿namespace PowerfulSpace.PollingSystem.Entities
+{
+    public class PollAnswer
+    {
+        public string Title { get; set; } = null!;
+
+        public int Votes { get; set; }
+
+        public double Procents { get; set; }
+    }
+
+}
